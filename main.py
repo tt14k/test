10fa8d2ac12a72ad3e1678ac366e0761a63633ae
tt14k <overtakeKing1416@gmail.com>
2024-08-25 15:41:01 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 def main():
     return render_template('index.html')
 
+@app.route('/option')
+def main():
+    return render_template('option.html')
+
 if __name__ == '__main__':
     app.debug = True  # デバッグモード有効化
     app.run(debug=True)
